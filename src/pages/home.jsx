82a1d8405f6df7167a9cc676a2af01a8bfe9a7d1
Javier <javiergo.2502@gmail.com>
2022-01-27 20:41:52 -0500

    Home

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -2,12 +2,9 @@ import "../styles/home.css"
 
 const Home = () => {
     return(
-        <>
-        <div class="scene">
-            <h1>Simple magic!</h1>
-            <div class="magic"></div>
-        </div>
-        </>
+        <main className="home">
+            <span className="presentation">Hola, Soy Javier!</span>
+        </main>
     )
 }
 
